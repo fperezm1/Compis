f874dddc3985eec0c1b5603adfda2b94e0f6c118
clopezr9 <clopezr9@eafit.edu.co>
2020-05-31 16:26:19 -0500

    Somethings

--- src/main/java/co/edu/eafit/dis/st0270/s20201/fac/grammar/EnumGrammarSymbol.java
+++ src/main/java/co/edu/eafit/dis/st0270/s20201/fac/grammar/EnumGrammarSymbol.java
@@ -1,5 +1,5 @@
 package co.edu.eafit.dis.st0270.s20201.fac.grammar;
 
 public enum EnumGrammarSymbol {
-    I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, I16,  EndOfString, 
+    I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, I16,  EndOfString
 }
